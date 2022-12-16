641b21fd83601bd3d363895349af7faf8ce4d6ce
Abdessamadbda <abdessamadelbada@gmail.com>
2022-12-17 00:10:12 +0100

    commit

---
+++ js/register.js
@@ -0,0 +1,8 @@
+
+function confirmPwd(){
+    let password=document.getElementById('password').value;
+let confirmPassword=document.getElementById('confirm-password').value;
+    if(password!=confirmPassword){
+        alert("hhhhhh");
+    }
+}
