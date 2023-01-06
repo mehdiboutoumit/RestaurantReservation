31b9108e21f5c7399e718daf916ec19596d7e06f
HassanBouighamdane <117985250+HassanBouighamdane@users.noreply.github.com>
2023-01-06 16:56:58 +0100

    Delete js directory

--- js/register.js
+++
@@ -1,8 +0,0 @@
-
-function confirmPwd(){
-    let password=document.getElementById('password').value;
-let confirmPassword=document.getElementById('confirm-password').value;
-    if(password!=confirmPassword){
-        alert("hhhhhh");
-    }
-}
